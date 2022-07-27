98250298b2e811354b23f4fd4d1d6cdaa38e7ae2
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2022-07-27 19:43:32 +0200

    added error messages

--- hopaas_client/utils/valid_properties.py
+++ hopaas_client/utils/valid_properties.py
@@ -1,14 +1,15 @@
 from typing import Union
 
 
-# FIXME: add uniqueness check for propertiy keys
+# FIXME: add uniqueness check for property keys
 def valid_properties(properties: Union[dict, None],
                      allow_none: bool = True) -> Union[dict, None]:
     if properties is None:
         if allow_none:
             valid_props = None
         else:
-            raise ValueError("pippo")   # TODO: add error message
+            raise ValueError("None value not supported, please pass the "
+                             "study properties through a dictionary.")
     elif isinstance(properties, dict):
         valid_props = dict()
         for k, v in properties.items():
@@ -16,5 +17,6 @@ def valid_properties(properties: Union[dict, None],
                 k = k[1:]
             valid_props[k] = v
     else:
-        raise ValueError("pippo")   # TODO: add error message
+        raise ValueError("Properties should be passed as dictionary, "
+                         f"instead {type(properties)} passed.")
     return valid_props
